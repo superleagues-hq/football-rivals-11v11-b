45c949e3831e608b35ae5013047bb66b0a6fd3e6
TEST <test@macbookpro.mynetworksettings.com>
2026-09-22 04:18:47 -0400

    Tuesday update (uncommitted from prior run)

--- archive/week-2/register.js
+++
@@ -1,6 +0,0 @@
-// Register the service worker for offline access
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("sw.js").catch(() => {});
-  });
-}
